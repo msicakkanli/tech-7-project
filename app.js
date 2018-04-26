be17dcb5199cc3543a6d2b5393b133e67ba512f3
msicakkanli <msicakkanli@protein.tech>
2018-04-26 22:08:42 +0300

    add config

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const config = require('./app/config');
 
 const app = express();
 
 app.get('/', (req, res) => {
    res.send('<h1>Express is working</h1>')
+  
   });
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
+    console.log(config.consumer_key);
 });
