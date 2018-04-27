b00895ad58fcc6f3d1b44bf027f4a874f8cbf03e
msicakkanli <msicakkanli@protein.tech>
2018-04-27 11:32:25 +0300

    add procfile

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const app = express();
 
 app.get('/', (req, res) => {
    res.send('<h1>Express is working</h1>')
-  
   });
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
-    console.log(config.consumer_key);
+    
 });
