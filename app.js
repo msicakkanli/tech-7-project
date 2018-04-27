fa5f06ba8dca2fd747f1c67a4fda6f17d89685e6
msicakkanli <msicakkanli@protein.tech>
2018-04-27 12:54:28 +0300

    add pug

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const config = require('./app/config');
+const pug = require('pug');
 
 const app = express();
-let server = require('https').Server(app);
-var port = process.env.PORT || 8000
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-   res.send('<h1>Express is working</h1>')
+   res.render('index')
   });
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
 });
 
-server.listen(port, function() {
-    console.log("App is running on port " + port);
-});
